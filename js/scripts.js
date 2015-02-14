70f70971d0ddb7d58659a5423e52792e4ac78eab
Naro Yessaian <naroyessaian@Naros-MacBook-Air.local>
2015-02-14 15:12:40 -0800

    Update 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 
+/*Check All*/
+
+function CheckAll(chk)
+{
+for (i = 0; i < chk.length; i++)
+chk[i].checked = true ;
+}
+
+function UnCheckAll(chk)
+{
+for (i = 0; i < chk.length; i++)
+chk[i].checked = false ;
+}
+
+
