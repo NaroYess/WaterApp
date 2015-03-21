d0a04e39fa8a8f72daafc59d7069814a57eef29d
Naro Yessaian <naroyessaian@Naros-MacBook-Air.local>
2015-03-21 13:15:48 -0700

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-<<<<<<< HEAD
 
+/* Schedules a local notification to be triggered after 5 seconds
+window.plugins.localNotification.add({
+    fireDate        : Math.round(new Date().getTime()/1000 + 5),
+    alertBody       : "This is a local notification.",
+    action          : "View",
+    repeatInterval  : "daily",
+    soundName       : "beep.caf",
+    badge           : 0,
+    notificationId  : 123,
+    foreground      : function(notificationId){ 
+        alert("Hello World! This alert was triggered by notification " + notificationId); 
+    },
+    background  : function(notificationId){
+        alert("Hello World! This alert was triggered by notification " + notificationId);
+    }           
+});
+
+// cancel notificationId = 1234
+window.plugins.localNotification.cancel(1234, callback);
+
+// cancel all notifications
+window.plugins.localNotification.cancelAll(callback);
+
+// set badge number to 3
+window.plugins.localNotification.setBadgeNumber(3);
 /*Check All*/
 
 function CheckAll(chk)
@@ -15,12 +39,4 @@ chk[i].checked = false ;
 }
 
 
-/*homework*/
 
-=======
- $('#myButton').on('click', function () {
-    var $btn = $(this).button('loading')
-    // business logic...
-    $btn.button('reset')
-  })
->>>>>>> master
